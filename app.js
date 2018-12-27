7fd6ff472d2a220ec829210543ed79c194aeab03
Joseph-Kung <joseph.kung@hotmail.com>
2018-12-27 10:42:43 -0800

    Add tweets and users routes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const bodyParser = require('body-parser');
 const app = express();
 const db = require('./config/keys').mongoURI;
 
@@ -8,6 +11,11 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello MERN World!!!'));
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Sever is running on port ${port}`));
